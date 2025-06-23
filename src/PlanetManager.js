49e3b8b2ed1a926ea934f76b25c2154186d1624f
Sebastian Stevens <sebastian.stevens19@googlemail.com>
2025-06-23 03:28:08 +0200

    Add GPU-based terrain shader

--- src/PlanetManager.js
+++ src/PlanetManager.js
@@ -9,34 +9,49 @@ import HeightmapStack, { FBMModifier, DomainWarpModifier, TerraceModifier, Cliff
 import { getCameraFrustum } from './utils/BoundingUtils.js';
 
 export default class PlanetManager {
-  constructor(scene, radius = 1) {
+  constructor(scene, radius = 1, useGPU = true) {
     this.scene = scene;
+    this.useGPU = useGPU;
 
     const seed = 1234;
-    const fnl = new FastNoiseLite(seed);
-    fnl.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
+    this.seed = seed;
+    let fnl;
+    if (!this.useGPU) {
+      fnl = new FastNoiseLite(seed);
+      fnl.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
+    }
 
-    this.heightStack = new HeightmapStack(seed);
-    this.domainWarp = new DomainWarpModifier(fnl, 0.2);
-    this.fbm = new FBMModifier(fnl, 1.0, 1.2, 5);
-    this.terrace = new TerraceModifier(8, 0.8);
-    this.cliff = new CliffModifier(0.25, 2.2);
+    if (this.useGPU) {
+      this.heightStack = { getHeight() { return 0; } };
+    } else {
+      this.heightStack = new HeightmapStack(seed);
+      this.domainWarp = new DomainWarpModifier(fnl, 0.2);
+      this.fbm = new FBMModifier(fnl, 1.0, 1.2, 5);
+      this.terrace = new TerraceModifier(8, 0.8);
+      this.cliff = new CliffModifier(0.25, 2.2);
 
-    this.modifiers = [this.domainWarp, this.fbm, this.terrace];
-    for (const m of this.modifiers) this.heightStack.add(m);
+      this.modifiers = [this.domainWarp, this.fbm, this.terrace];
+      for (const m of this.modifiers) this.heightStack.add(m);
 
-    this.useDomainWarp = true;
-    this.useTerrace = true;
-    this.useCliff = false;
+      this.useDomainWarp = true;
+      this.useTerrace = true;
+      this.useCliff = false;
+    }
 
     this.builder = new GeometryBuilder(this.heightStack, radius);
     this.lod = new ChunkLODController();
     this.chunks = [];
+    this.terrainMaterial = createTerrainMaterial({
+      seed,
+      radius,
+      amplitude: 0.3,
+      frequency: 1.2,
+    });
 
     const faces = ['px', 'nx', 'py', 'ny', 'pz', 'nz'];
     for (const face of faces) {
       const chunk = new FaceChunk(face, this.builder, 32);
-      chunk.createMesh(createTerrainMaterial());
+      chunk.createMesh(this.terrainMaterial);
       chunk.addToScene(scene);
       this.chunks.push(chunk);
     }
@@ -61,12 +76,17 @@ export default class PlanetManager {
     terraceSteps,
     terraceRange,
   }) {
-    if (amplitude !== undefined) this.fbm.amplitude = amplitude;
-    if (frequency !== undefined) this.fbm.frequency = frequency;
-    if (octaves !== undefined) this.fbm.octaves = octaves;
-    if (warpIntensity !== undefined) this.domainWarp.intensity = warpIntensity;
-    if (terraceSteps !== undefined) this.terrace.steps = terraceSteps;
-    if (terraceRange !== undefined) this.terrace.heightRange = terraceRange;
+    if (this.useGPU) {
+      if (amplitude !== undefined) this.terrainMaterial.uniforms.uAmplitude.value = amplitude;
+      if (frequency !== undefined) this.terrainMaterial.uniforms.uFrequency.value = frequency;
+    } else {
+      if (amplitude !== undefined) this.fbm.amplitude = amplitude;
+      if (frequency !== undefined) this.fbm.frequency = frequency;
+      if (octaves !== undefined) this.fbm.octaves = octaves;
+      if (warpIntensity !== undefined) this.domainWarp.intensity = warpIntensity;
+      if (terraceSteps !== undefined) this.terrace.steps = terraceSteps;
+      if (terraceRange !== undefined) this.terrace.heightRange = terraceRange;
+    }
   }
 
   setModifierEnabled(name, enabled) {
@@ -87,6 +107,8 @@ export default class PlanetManager {
   }
 
   _toggleModifier(mod, enabled) {
+    if (this.useGPU) return;
+
     const hasMod = this.heightStack.modifiers.includes(mod);
     if (enabled && !hasMod) {
       this.heightStack.modifiers.push(mod);
